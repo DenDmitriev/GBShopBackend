2b1a9098b7d24f3c0599e88d6850cea93bbd0543
DenDmitriev <dv.denstr@gmail.com>
2023-06-26 16:36:20 +0300

    Update configure.swift

--- Sources/App/configure.swift
+++ Sources/App/configure.swift
@@ -7,11 +7,6 @@ import Vapor
 public func configure(_ app: Application) async throws {
     // uncomment to serve files from /Public folder
     // app.middleware.use(FileMiddleware(publicDirectory: app.directory.publicDirectory))
-
-    // Configure custom hostname.
-    // app.http.server.configuration.hostname = "hostname"
-    // Configure custom port.
-    app.http.server.configuration.port = 80
     
     app.databases.use(.sqlite(.file("db.sqlite")), as: .sqlite)
 
