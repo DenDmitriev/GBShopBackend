9dca9a3cd20023e074bea8b685e0ff24699a1952
DenDmitriev <dv.denstr@gmail.com>
2023-06-26 13:29:19 +0300

    changer port
    set port to 80

--- Sources/App/configure.swift
+++ Sources/App/configure.swift
@@ -8,6 +8,8 @@ public func configure(_ app: Application) async throws {
     // uncomment to serve files from /Public folder
     // app.middleware.use(FileMiddleware(publicDirectory: app.directory.publicDirectory))
 
+    app.http.server.configuration.port = 80
+    
     app.databases.use(.sqlite(.file("db.sqlite")), as: .sqlite)
 
     app.migrations.add(CreateTodo())
