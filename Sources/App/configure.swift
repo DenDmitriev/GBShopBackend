b48897b3c19e5155f164839a748e4f4cf25f6b02
DenDmitriev <dv.denstr@gmail.com>
2023-06-27 08:51:16 +0300

    Update configure.swift

--- Sources/App/configure.swift
+++ Sources/App/configure.swift
@@ -8,6 +8,7 @@ public func configure(_ app: Application) async throws {
     // uncomment to serve files from /Public folder
     // app.middleware.use(FileMiddleware(publicDirectory: app.directory.publicDirectory))
 
+    print(app.directory)
     app.databases.use(.sqlite(.file("data/db.sqlite")), as: .sqlite)
 
     app.migrations.add(CreateTodo())
