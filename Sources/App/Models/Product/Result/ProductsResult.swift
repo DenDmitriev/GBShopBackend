2303e5bad41af7e42cc08eaf1fee8e4b0cf30961
DenDmitriev <dv.denstr@gmail.com>
2023-06-30 18:35:59 +0300

    Update ProductsResult.swift

--- Sources/App/Models/Product/Result/ProductsResult.swift
+++ Sources/App/Models/Product/Result/ProductsResult.swift
@@ -27,9 +27,13 @@ struct ProductsResult: Content {
         let id: UUID?
         let name: String
         let price: Float
+        let description: String
         
         static func fubric(_ product: Product) -> ProductResult {
-            return ProductResult(id: product.id, name: product.name, price: product.price)
+            return ProductResult(id: product.id,
+                                 name: product.name,
+                                 price: product.price,
+                                 description: product.description)
         }
     }
 }
