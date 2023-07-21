8cc6aacb46e2821e8a69b726ffdbdf616549c577
DenDmitriev <dv.denstr@gmail.com>
2023-07-21 11:53:46 +0300

    Update configure.swift

--- Sources/App/configure.swift
+++ Sources/App/configure.swift
@@ -8,9 +8,14 @@ public func configure(_ app: Application) async throws {
     // uncomment to serve files from /Public folder
     // app.middleware.use(FileMiddleware(publicDirectory: app.directory.publicDirectory))
 
-    app.databases.use(.sqlite(.file("db.sqlite")), as: .sqlite)
+    
+    let databasePath = "/data/db.sqlite"
+    print("database exists on path", databasePath, FileManager.default.fileExists(atPath: databasePath))
+    app.databases.use(.sqlite(.file(databasePath)), as: .sqlite)
 
     app.migrations.add(CreateTodo())
+    
+    try await app.autoMigrate()
 
     // register routes
     try routes(app)
