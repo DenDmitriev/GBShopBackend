3a75d94992f27595a94679bcf578bdbdaf599918
DenDmitriev <dv.denstr@gmail.com>
2023-07-21 10:46:28 +0300

    add func for create data directory

--- Sources/App/configure.swift
+++ Sources/App/configure.swift
@@ -8,8 +8,20 @@ public func configure(_ app: Application) async throws {
     // uncomment to serve files from /Public folder
     // app.middleware.use(FileMiddleware(publicDirectory: app.directory.publicDirectory))
 
+    if let workingDirectory = URL(string: app.directory.workingDirectory) {
+        let dataDirectory = workingDirectory.appendingPathComponent("data")
+        if !FileManager.default.fileExists(atPath: dataDirectory.path) {
+            do {
+                try FileManager.default.createDirectory(atPath: dataDirectory.path, withIntermediateDirectories: true)
+            } catch {
+                print(error.localizedDescription)
+            }
+        }
+    }
+    
 //    let databasePath = app.directory.workingDirectory + "data/db.sqlite"
     let databasePath = "/data/db.sqlite"
+    
     print("database exists on path", databasePath, FileManager.default.fileExists(atPath: databasePath))
     app.databases.use(.sqlite(.file(databasePath)), as: .sqlite)
 
