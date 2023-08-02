ddfda830b50722e63870bfd1f1db0f4351d760e4
DenDmitriev <dv.denstr@gmail.com>
2023-08-02 08:17:28 +0300

    update basket model in db

--- Sources/App/configure.swift
+++ Sources/App/configure.swift
@@ -8,19 +8,19 @@ public func configure(_ app: Application) async throws {
     // uncomment to serve files from /Public folder
     // app.middleware.use(FileMiddleware(publicDirectory: app.directory.publicDirectory))
 
-//    if let workingDirectory = URL(string: app.directory.workingDirectory) {
-//        let dataDirectory = workingDirectory.appendingPathComponent("data")
-//        if !FileManager.default.fileExists(atPath: dataDirectory.path) {
-//            do {
-//                try FileManager.default.createDirectory(atPath: dataDirectory.path, withIntermediateDirectories: true)
-//            } catch {
-//                print(error.localizedDescription)
-//            }
-//        }
-//    }
-//    let databasePath = app.directory.workingDirectory + "data/db.sqlite"
+    if let workingDirectory = URL(string: app.directory.workingDirectory) {
+        let dataDirectory = workingDirectory.appendingPathComponent("data")
+        if !FileManager.default.fileExists(atPath: dataDirectory.path) {
+            do {
+                try FileManager.default.createDirectory(atPath: dataDirectory.path, withIntermediateDirectories: true)
+            } catch {
+                print(error.localizedDescription)
+            }
+        }
+    }
+    let databasePath = app.directory.workingDirectory + "data/db.sqlite"
     
-    let databasePath = "/data/db.sqlite"
+//    let databasePath = "/data/db.sqlite"
     print("database exists on path", databasePath, FileManager.default.fileExists(atPath: databasePath))
     app.databases.use(.sqlite(.file(databasePath)), as: .sqlite)
 
@@ -30,7 +30,11 @@ public func configure(_ app: Application) async throws {
     app.migrations.add(CreateProduct())
     app.migrations.add(CreateReviews())
     app.migrations.add(CreateCategory())
-    app.migrations.add(CreateBasket())
+//    app.migrations.add(CreateBasket())
+    
+    let basketMigration = CreateBasket()
+    try await basketMigration.revert(on: app.db)
+    try await basketMigration.prepare(on: app.db)
     
     try await app.autoMigrate()
 
